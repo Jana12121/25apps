9b8520c89dadc6b07761b0a82a0103f60eb95e18
Jana12121 <jj3395210@gmail.com>
2024-08-14 13:50:10 +0200

    Scroll Indicator
    starting Scroll Indicator project

---
+++ src/components/scroll-indicator/ScrollIndicator.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export const ScrollIndicator = () => {
+  return <div className={styles.container}>ScrollIndicator</div>;
+};
