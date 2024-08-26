9806cc2a809f7bcd3745c942183006cf1fb8b5fc
Jana12121 <jj3395210@gmail.com>
2024-08-26 18:09:15 +0200

    scss layout
    temp save

--- src/components/tictactoe/TicTacToe.jsx
+++ src/components/tictactoe/TicTacToe.jsx
@@ -6,11 +6,15 @@ export const TicTacToe = () => {
   const [squares, setSquares] = useState(Array(9).fill(""));
   const [isXTurn, setIsXTurn] = useState(true);
   const [status, setStatus] = useState("");
+  const [firstPlayer, setFirstPlayer] = useState("X");
+  const [secondPlayer, setSecondPlayer] = useState("O");
 
   function handleClick(getCurrentSquare) {
     let copySquare = [...squares];
     if (getWinner(copySquare) || copySquare[getCurrentSquare]) return;
-    copySquare[getCurrentSquare] = isXTurn ? "X" : "O";
+    copySquare[getCurrentSquare] = isXTurn
+      ? firstPlayer[0].toUpperCase()
+      : secondPlayer[0].toUpperCase();
     setIsXTurn(!isXTurn);
     setSquares(copySquare);
   }
@@ -46,7 +50,11 @@ export const TicTacToe = () => {
     } else if (getWinner(squares)) {
       setStatus(`Winner is ${getWinner(squares)}`);
     } else {
-      setStatus(`Next Player is ${isXTurn ? "X" : "O"}`);
+      setStatus(
+        `Next Player is ${
+          isXTurn ? firstPlayer[0].toUpperCase() : secondPlayer[0].toUpperCase()
+        }`
+      );
     }
   }, [squares, isXTurn]);
 
@@ -55,27 +63,58 @@ export const TicTacToe = () => {
     setSquares(Array(9).fill(""));
   }
 
+  function handleFirstPlayer(e) {
+    setFirstPlayer(e.target.value);
+  }
+
+  function handleSecondPlayer(e) {
+    setSecondPlayer(e.target.value);
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.row}>
-        <Square value={squares[0]} onClick={() => handleClick(0)} />
-        <Square value={squares[1]} onClick={() => handleClick(1)} />
-        <Square value={squares[2]} onClick={() => handleClick(2)} />
+    <div className={styles.mainDiv}>
+      <div className={styles.inputsDiv}>
+        <label>
+          1 Player:{" "}
+          <input
+            type="text"
+            placeholder="player name"
+            value={firstPlayer}
+            onChange={handleFirstPlayer}
+          />
+        </label>
+        <label>
+          2 Player:{" "}
+          <input
+            type="text"
+            placeholder="player name"
+            value={secondPlayer}
+            onChange={handleSecondPlayer}
+          />
+        </label>
       </div>
-      <div className={styles.row}>
-        <Square value={squares[3]} onClick={() => handleClick(3)} />
-        <Square value={squares[4]} onClick={() => handleClick(4)} />
-        <Square value={squares[5]} onClick={() => handleClick(5)} />
+
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <Square value={squares[0]} onClick={() => handleClick(0)} />
+          <Square value={squares[1]} onClick={() => handleClick(1)} />
+          <Square value={squares[2]} onClick={() => handleClick(2)} />
+        </div>
+        <div className={styles.row}>
+          <Square value={squares[3]} onClick={() => handleClick(3)} />
+          <Square value={squares[4]} onClick={() => handleClick(4)} />
+          <Square value={squares[5]} onClick={() => handleClick(5)} />
+        </div>
+        <div className={styles.row}>
+          <Square value={squares[6]} onClick={() => handleClick(6)} />
+          <Square value={squares[7]} onClick={() => handleClick(7)} />
+          <Square value={squares[8]} onClick={() => handleClick(8)} />
+        </div>
+        <h1 className={styles.status}>{status}</h1>
+        <button className={styles.restartButton} onClick={handleRestart}>
+          Restart Game
+        </button>
       </div>
-      <div className={styles.row}>
-        <Square value={squares[6]} onClick={() => handleClick(6)} />
-        <Square value={squares[7]} onClick={() => handleClick(7)} />
-        <Square value={squares[8]} onClick={() => handleClick(8)} />
-      </div>
-      <h1 className={styles.status}>{status}</h1>
-      <button className={styles.restartButton} onClick={handleRestart}>
-        Restart Game
-      </button>
     </div>
   );
 };
